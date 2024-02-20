d7d8c87e6baa0c9b17b39c6f8b44ed2ea0492510
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-02-20 16:35:01 +0530

    20/02/2024

---
+++ src/CodeChef/StringRev.java
@@ -0,0 +1,10 @@
+package CodeChef;
+
+import java.util.Scanner;
+
+public class StringRev {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        char[] str=new char[3];
+    }
+}
--- src/pattarn/FibonnaciPatrn.java
+++ src/pattarn/FibonnaciPatrn.java
@@ -1,4 +1,39 @@
 package pattarn;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class FibonnaciPatrn {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of patrn:");
+        int n=sc.nextInt();
+//        for (int i=1;i<=n;i++)
+//        {
+//            for(int j=1;j<=i;j++)
+//            {
+//                System.out.print("@ ");
+//            }
+//            System.out.println();
+//        }
+
+
+        //uncomment these two line
+//        int a=0;
+//        int b=1;
+        for (int i=1;i<=n;i++)
+        {
+            //one time plz comment these two line
+            int a=0;
+            int b=1;
+            for(int j=1;j<=i;j++)
+            {
+                int c=a+b;
+                System.out.print(c+" ");
+                a=b;
+                b=c;
+            }
+            System.out.println();
+        }
+    }
 }
