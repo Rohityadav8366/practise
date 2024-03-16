02c9fee8593a26bce9eb8fc7bb023885c0b8ea2f
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-03-16 15:33:56 +0530

    methode

--- src/Multitheading/RunnableInterface.java
+++ src/Multitheading/RunnableInterface.java
@@ -1,4 +1,44 @@
 package Multitheading;
+// This is another methode to achives multithreading
+//the Runnable interface is implements to attach to the thread so that is Class MY will be execute
+//basically thread is execute directly so using the instance of the class
+//and attach to the thread .
+//Note -> thread is direct executed and runnable interface is executed by the help of <Thread>.
+//class My implements Runnable
+//{
+//    public void run()
+//    {
+//        int i=1;
+//        while(i<50)
+//        {
+//            System.out.println(i+" HELLO");
+//            i++;
+//        }
+//    }
+//}
+public class RunnableInterface implements Runnable
+{
+    // without using class use methode
+    public void run()
+    {
+        int i=0;
+        while(i<50)
+        {
+            System.out.println(i+" HELLO");
+            i++;
+        }
+    }
 
-public class RunnableInterface {
+    public static void main(String[] args) {
+       // My m=new My();
+        RunnableInterface m=new RunnableInterface();
+        Thread t=new Thread(m);
+        t.start();
+        int i=1;
+        while(i<50)
+        {
+            System.out.println(i+" ROHIT");
+            i++;
+        }
+    }
 }
---
+++ src/Multitheading/ThreadMethode.java
@@ -0,0 +1,5 @@
+package Multitheading;
+
+public class ThreadMethode {
+
+}
