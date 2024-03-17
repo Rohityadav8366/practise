e64b80749f904182b8e0df48aa9a05e5c61afc22
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-03-17 10:30:16 +0530

    multithread

--- src/Multitheading/ThreadMethode.java
+++ src/Multitheading/ThreadMethode.java
@@ -1,5 +1,51 @@
 package Multitheading;
 
-public class ThreadMethode {
+class MyThread extends Thread
+{
+//    public MyThread()
+//    {
+//        super(name);
+//    }
+    public void run()
+    {
+        int count=1;
+        while(true){
+            System.out.println(count++ +" HELLO");
+//            try
+//            {
+//               Thread.sleep(100);
+//            }
+//            catch (InterruptedException e){
+//                System.out.println(e);
+//            }
+        }
+    }
+}
+public class ThreadMethode
+{
+    public static void main(String[] args) throws Exception
+    {
+        MyThread t=new MyThread();
+//        System.out.println(t.getId());
+//        System.out.println(t.getName());
+//        System.out.println(t.getPriority());
+//        t.start();
+//        System.out.println(t.getState());
+//        System.out.println(t.isAlive());
+//        System.out.println(t.isDaemon());
+            t.setDaemon(true);
+            t.start();
+          // try{Thread.sleep(10);}catch(Exception e){}
+        Thread mainThread=Thread.currentThread();
+        //join() methode is the wait other thread to terminate
+      // mainThread.join();
+        int count=1;
+        while(true) {
+            System.out.println(count++ + " rohit");
 
+            // yield() methode is the methode which is provided other thread more CPU time
+
+            Thread.yield();
+        }
+    }
 }
