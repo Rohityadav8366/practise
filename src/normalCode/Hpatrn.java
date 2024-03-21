3e37315b1cb355aea785ca18747e2363c1d05c44
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-03-21 23:15:41 +0530

    H pattern

--- src/normalCode/Hpatrn.java
+++ src/normalCode/Hpatrn.java
@@ -5,9 +5,9 @@ public class Hpatrn
     public static void main(String[] args) {
         int i, j;
 
-        for (i = 1; i <= 7; i++) {
-            for (j = 1; j <= 7; j++) {
-                if (j == 1 || j == 7 || (i == 4 && (j >= 2 && j <= 6))) {
+        for (i = 1; i <= 8; i++) {
+            for (j = 1; j <= 8; j++) {
+                if (j == 1 || j == 8 || (i == 4 && (j >= 2 && j <= 6))) {
                         System.out.print("* ");
                 }
                 else {
--- src/pattarn/H.java
+++ src/pattarn/H.java
@@ -9,9 +9,12 @@ public class H {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++)
             {
-                if((j==1&&j==n)||(i==n/2))
+                if((j==1||j==n)||(i==(n+1)/2))
                 {
                     System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
                 }
             }
             System.out.println();
