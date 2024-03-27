aa48e2a35c27e74a0c6a8752e2e8d2a6d26367f5
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-03-27 19:27:47 +0530

    generic

---
+++ src/DSA/ArrayList.java
@@ -0,0 +1,12 @@
+package DSA;
+
+import java.util.Collection;
+
+public class ArrayList {
+    public static void main(String[] args) {
+        ArrayList number=new ArrayList();
+
+        System.out.println(number);
+    }
+
+}
