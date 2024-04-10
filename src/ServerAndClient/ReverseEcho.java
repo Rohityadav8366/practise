f75db2bb3e5b1c126e4d749d6e0dc3cb7ee4aac7
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-04-10 17:29:20 +0530

    text

--- src/ServerAndClient/ReverseEcho.java
+++ src/ServerAndClient/ReverseEcho.java
@@ -1,2 +1,46 @@
-package ServerAndClient;public class ReverseEcho {
+package ServerAndClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.StringReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ReverseEcho {
+    public static void main(String[] args) throws Exception
+    {
+        ServerSocket ss=new ServerSocket(2000);
+        Socket stk=ss.accept();
+        BufferedReader br=new BufferedReader(new InputStreamReader(stk.getInputStream()));
+        PrintStream ps=new PrintStream(stk.getOutputStream());
+        String msg;
+        StringBuilder sb;
+        do {
+                msg=br.readLine();
+                 sb=new StringBuilder(msg);
+                sb.reverse();
+                msg=sb.toString();
+                ps.println(msg);
+        }while(!msg.equals("dne"));
+    }
+}
+
+
+class Client {
+    public static void main(String[] args) throws Exception
+    {
+        Socket stk=new Socket("2620:119:35::35",2000);
+        BufferedReader keyb=new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br=new BufferedReader(new InputStreamReader(stk.getInputStream()));
+        PrintStream ps=new PrintStream(stk.getOutputStream());
+        String msg;
+
+        do {
+            msg=keyb.readLine();
+            ps.println(msg);
+            msg=br.readLine();
+            System.out.println("From server "+msg );
+        }while(!msg.equals("dne"));
+    }
 }
--- src/Stream/Serialization.java
+++ src/Stream/Serialization.java
@@ -1,2 +1,55 @@
-package Stream;public class Serialization {
+package Stream;
+
+import java.io.*;
+
+class Student implements Serializable
+{
+    private int rollNo;
+    private  String name;
+    private float avg;
+    private String dept;
+    public static int Data=10;//it is not send into file serilizable is not access the static and transient keyword;
+
+    public transient int t;//not send into file
+    public Student(){}
+    public Student(int r,String n,float a,String d)
+    {
+        rollNo=r;
+        name=n;
+        avg=a;
+        dept=d;
+        Data=500;
+        t=500;
+    }
+    public String toString()
+    {
+        return "\nStudent Detail\n"+
+                "\nRollNO "+rollNo+
+                "\nName "+name+
+                "\nAverage "+avg+
+                "\nDept "+dept+
+                "\nData "+Data+
+                "\nTransient "+t+"\n";
+    }
+}
+public class Serialization
+{
+    public static void main(String[] args) throws Exception
+    {
+        // to write
+//        FileOutputStream fos=new FileOutputStream("D:\\java pro\\practice\\src\\Stream\\Stream4.txt");
+//        ObjectOutputStream oos=new ObjectOutputStream(fos);
+//        Student s=new Student(10,"Rohit",75.6f,"CSE");
+//        oos.writeObject(s);
+//        fos.close();
+//        oos.close();
+
+        // to read
+        FileInputStream fos=new FileInputStream("D:\\java pro\\practice\\src\\Stream\\Stream4.txt");
+        ObjectInputStream ois=new ObjectInputStream(fos);
+        Student s=(Student) ois.readObject();
+        System.out.println(s);
+        fos.close();
+        ois.close();
+    }
 }
