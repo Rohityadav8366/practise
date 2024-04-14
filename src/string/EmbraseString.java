e1062623c159db9ba544bc86be6e5169a2e69d89
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-04-14 23:49:13 +0530

    14/4/2024

--- src/string/EmbraseString.java
+++ src/string/EmbraseString.java
@@ -5,9 +5,15 @@ import java.util.Scanner;
 public class EmbraseString {
     public static void main(String[] args) {
        Scanner sc=new Scanner(System.in);
-       String[] str1=new String[1];
-       String[] str2=new String[2];
+       String str1=sc.nextLine();
+       String str2=sc.nextLine();
+            for(int i=0;i<=n;i++) {
+                str1.charAt(i);
+                str2.charAt(i);
+            }
+            for(int i=0;i<=0;i++)
+            {
 
-
+            }
     }
 }
