f95bc83891608c1033298a57964c9fa95128a5ec
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-04-18 17:00:06 +0530

    printing k pattern

---
+++ src/pattarn/Kpattern.java
@@ -0,0 +1,23 @@
+package pattarn;
+
+import java.util.Scanner;
+
+public class Kpattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                if(j==1||i==3&&j==2||(i==2||i==4)&&j==4||(i==1||i==5)&&j==5)
+                {
+                    System.out.print("*");
+                }
+                else{
+                System.out.print(" ");}
+            }
+            System.out.println();
+        }
+    }
+}
