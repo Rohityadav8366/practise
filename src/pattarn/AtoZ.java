cf8cae1c57ca2aec1185188bf268a1243343b362
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-04-26 03:12:04 +0530

    KL

--- src/pattarn/AtoZ.java
+++ src/pattarn/AtoZ.java
@@ -207,22 +207,57 @@ public class AtoZ {
 //        }
 
         //J
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<m;j++)
-            {
-                if(j==5||(j==1&&i==8)||(j==1&&i==7)||(j==2&&i==9)||(j==3&&i==9)||(j==4&&i==9))
-                {
-                    System.out.print("*");
-                }
-                else
-                {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+//        for(int i=1;i<m;i++)
+//        {
+//            for(int j=1;j<m;j++)
+//            {
+//                if(j==5||(j==1&&i==8)||(j==1&&i==7)||(j==2&&i==9)||(j==3&&i==9)||(j==4&&i==9))
+//                {
+//                    System.out.print("*");
+//                }
+//                else
+//                {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
 
-        //K
-        }
+        //k
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=5;j++)
+//            {
+//                if(j==1||i==3&&j==2||(i==2||i==4)&&j==4||(i==1||i==5)&&j==5)
+//                {
+//                    System.out.print("*");
+//                }
+//                else{
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
+
+
+     //L
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=5;j++)
+//            {
+//                if(j==1||i==5)
+//                {
+//                    System.out.print("*");
+//                }
+//                else{
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
+
+
+        //M
+
+     }
 }
