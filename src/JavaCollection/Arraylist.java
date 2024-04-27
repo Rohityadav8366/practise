2742a717bf6ab4732518e1f84954db6f103b5902
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-04-27 19:17:26 +0530

    learning collection

--- src/JavaCollection/Arraylist.java
+++ src/JavaCollection/Arraylist.java
@@ -2,16 +2,21 @@ package JavaCollection;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Arraylist {
     public static void main(String[] args) {
        ArrayList<Integer> al =new ArrayList<>();
-       Arraylist al1=new Arraylist();
+       ArrayList<Integer> al1=new ArrayList<>(List.of(23,24,13,45,65,20,30));
        al.add(0,10);
        al.add(1,20);
        al.add(2,30);
        al.add(3,40);
-
+        al.add(23);
+        al.add(54);
+        al1.addAll(al);
         System.out.println(al);
+        System.out.println(al1);
+        System.out.println(al.indexOf(30));
     }
 }
