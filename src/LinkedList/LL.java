0f301dbef508520a976c74a10b24ea54edd8a18e
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-05-08 20:31:56 +0530

    delete

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -30,6 +30,72 @@ public class LL {
         tail=node;
         size++;
     }
+    public void insert(int val,int index)
+    {
+        if(index==0)
+        {
+            insertFirst(val);
+            return;
+        }
+        if(index==size)
+        {
+            insertLast(val);
+            return;
+        }
+        Node temp=head;
+        for(int i=1;i<index;i++)
+        {
+            temp=temp.next;
+        }
+        Node node=new Node(val,temp.next);
+        temp.next=node;
+        size++;
+    }
+    //delete First index
+    public int deleteFirst()
+    {
+        System.out.println("delete from first index:");
+        int val=head.val;
+        head=head.next;
+        if(head==null){
+            tail=null;
+        }
+        size--;
+        return val;
+    }
+    public int deleteLast(){
+        System.out.println("delete from last index");
+        if (size<=1)
+        {
+            return deleteFirst();
+        }
+        Node secondLast=get(size-2);
+        int val=tail.val;
+        tail=secondLast;
+        tail.next=null;
+        return val;
+    }
+    public Node get(int index){
+        Node node=head;
+        for(int i=0;i<index;i++)
+        {
+            node=node.next;
+        }
+        return node;
+    }
+    public int delete(int index)
+    {
+        if(index==0){
+            return deleteFirst();
+        }
+        if(index==size-1){
+            return deleteLast();
+        }
+        Node prev=get(index-1);
+        int val=prev.next.val;
+        prev.next=prev.next.next;
+        return val;
+    }
     public void display()
     {
         Node temp=head;
--- src/LinkedList/main.java
+++ src/LinkedList/main.java
@@ -5,14 +5,23 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of LL");
         int n=sc.nextInt();
         LL linked =new LL();
+        System.out.println("Enter the element:");
         for(int i=0;i<n;i++)
         {
             int num=sc.nextInt();
             //linked.insertFirst(num);
             linked.insertLast(num);
         }
+//        System.out.println("Enter the val and index");
+//        int val=sc.nextInt();
+        int index=sc.nextInt();
+//       // linked.insert(val,index);
+       // linked.deleteLast();
+        //linked.deleteFirst();
+        linked.delete(index);
         linked.display();
     }
 }
