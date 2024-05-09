c7fa4a8571bb70eda1536cd9766615a5c24a2c95
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-05-10 01:47:13 +0530

    jd

--- src/CodeChef/ChefAndString.java
+++ src/CodeChef/ChefAndString.java
@@ -6,7 +6,9 @@ public class ChefAndString {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int count=0;
+        System.out.println("enter the value of n:");
         int n=sc.nextInt();
+        System.out.println("Enter the string:");
         sc.nextLine();
         String str=sc.nextLine();
         for(int i=0;i<=n-1;i++)
