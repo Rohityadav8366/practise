9e18c3f2f4fa29383a9ad1440b1721456f5bb2ed
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-05-13 00:19:33 +0530

    Haker earth que 1

---
+++ src/array/MinmiumLength.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.util.Scanner;
+
+public class MinmiumLength
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the N :");
+        int n=sc.nextInt();
+        int[] A=new int[n];
+        int [] B=new int[n];
+        System.out.println("Enter the element array A");
+        for(int i=0;i<n;i++)
+        {
+            A[i]=sc.nextInt();
+        }
+        System.out.println("Enter the element array B");
+        for(int i=0;i<n;i++)
+        {
+            B[i]=sc.nextInt();
+        }
+
+    }
+}
