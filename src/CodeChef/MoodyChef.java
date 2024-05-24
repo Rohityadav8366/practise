c6aeec0ea3aadc744f15e84254e7369cec10860e
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-05-24 14:46:57 +0530

    code

---
+++ src/CodeChef/MoodyChef.java
@@ -0,0 +1,32 @@
+package CodeChef;
+
+import java.util.Scanner;
+
+public class MoodyChef {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the value of N,L,R");
+        int n=sc.nextInt();
+        int l=sc.nextInt();
+        int r=sc.nextInt();
+        System.out.println("Enter the value of Array :)");
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int mincount=0,mixcount=0;
+        for(int i=0;i< arr.length;i++)
+        {
+            if(arr[i]==l||arr[i]==r)
+            {
+                mixcount++;
+            }
+            else
+            {
+                mincount--;
+            }
+        }
+        System.out.println(mixcount+""+mincount);
+    }
+}
