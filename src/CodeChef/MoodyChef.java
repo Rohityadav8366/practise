9586206aaa43b6aa1d5b4c68b44c951de2df991a
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-05-24 21:44:01 +0530

    sh

--- src/CodeChef/MoodyChef.java
+++ src/CodeChef/MoodyChef.java
@@ -15,18 +15,19 @@ public class MoodyChef {
         {
             arr[i]=sc.nextInt();
         }
-        int mincount=0,mixcount=0;
+        int mincount=0;
+        int maxcount=0;
         for(int i=0;i< arr.length;i++)
         {
-            if(arr[i]==l||arr[i]==r)
+            if(arr[i]>=l&&arr[i]<=r)
             {
-                mixcount++;
+                 maxcount+=1;
             }
             else
             {
-                mincount--;
+                mincount-=1;
             }
         }
-        System.out.println(mixcount+""+mincount);
+        System.out.println(maxcount+""+mincount);
     }
 }
