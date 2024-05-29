ba027b3b11172baa4bd38ef6f7795e37f7a37aac
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-05-29 23:58:08 +0530

    Dominenet

---
+++ src/CodeChef/DominenetElement.java
@@ -0,0 +1,26 @@
+package CodeChef;
+
+import java.util.Scanner;
+
+public class DominenetElement {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the n:");
+        int n=sc.nextInt();
+        System.out.println("Enter the element of array:");
+        int [] arr=new int[n];
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length-1;i++)
+        {
+            if(arr[i]==arr[i+1])
+            {
+                count++;
+            }
+        }
+
+    }
+}
