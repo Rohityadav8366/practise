ba7433056fdb7394d698dad87af3efc450881be9
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-06-01 01:57:50 +0530

    NonAdsF

---
+++ src/CodeChef/NonAdaFlips.java
@@ -0,0 +1,29 @@
+package CodeChef;
+
+import java.util.Scanner;
+
+public class NonAdaFlips {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the  value n:");
+        int n=sc.nextInt();
+        String[] str =new String[n];
+        int o=0,j=0;
+        for(int i=0;i<n;i++)
+        {
+            str[i]=sc.next();
+            if(str[i].equals('0'))
+            {
+                o++;
+            }
+            else {
+                j++;
+            }
+        }
+        if(j>o) {
+            for (int i = 0; i < str.length; i++) {
+
+            }
+        }
+    }
+}
