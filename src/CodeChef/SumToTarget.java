9e482e33532dd556f638285c20887d3a4348a847
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-06-03 02:48:44 +0530

    sumhfguer

--- src/CodeChef/SumToTarget.java
+++ src/CodeChef/SumToTarget.java
@@ -13,16 +13,35 @@ public class SumToTarget {
         {
             arr[i]=sc.nextInt();
         }
-        for(int i=0;i< arr.length;i++)
-        {
-            for(int j=i+1;j< arr.length;j++)
+        //show the index leet code problm
+//        for(int i=0;i< arr.length;i++)
+//        {
+//            for(int j=i+1;j< arr.length;j++)
+//            {
+//                sum=arr[i]+arr[j];
+//                if(sum==tar)
+//                {
+//                    System.out.println("["+i+","+j+"]");
+//                }
+//            }
+
+
+        // show the no.of element add to achieve target codechef problm
+        int count=0;
+        while(i<n)
+      for(int j=0;j<arr.length;j++)
             {
-                sum=arr[i]+arr[j];
+                if(sum<7){
+                    sum+=arr[j];
+                    count++;
+                }
                 if(sum==tar)
                 {
-                    System.out.println("["+i+","+j+"]");
+                    System.out.println(count);
                 }
             }
+            sum=0;
+           count=0;
         }
     }
 }
