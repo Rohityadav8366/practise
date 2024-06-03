939494f70f5641f95b19eb6496bc8f5e9c0e057c
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-06-03 23:57:22 +0530

    Runway

--- src/CodeChef/CardSwap.java
+++ src/CodeChef/CardSwap.java
@@ -8,21 +8,20 @@ public class CardSwap {
         System.out.println("enter the value of n");
         int n=sc.nextInt();
         int[] arr=new int[n];
+        int [] arr1=new int[n];
         int count=1;
         System.out.println("Enter the element");
         for(int i=0;i<n;i++)
         {
             arr[i]= sc.nextInt();
         }
-        for(int i=0;i<arr.length;i++)
+        for(int i=0;i<arr.length-1;i++)
         {
-            for (int j=i+1;j< arr.length;j++)
+            if(arr[i]!=arr[i+1])
             {
-                if(arr[i]==arr[j])
-                {
-                    count++;
-                }
+                arr1[i]=arr[i];
             }
+            System.out.println(arr1[i]);
         }
         System.out.println(count);
     }
