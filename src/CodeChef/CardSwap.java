64a90e47672df821c730f8044a706e9700badce7
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-06-04 01:25:53 +0530

    Runway

--- src/CodeChef/CardSwap.java
+++ src/CodeChef/CardSwap.java
@@ -1,28 +1,38 @@
 package CodeChef;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class CardSwap {
+         public static int maxPeopleInOffice(int[] arr) {
+            HashSet<Integer> currentInside = new HashSet<>();
+            int Count = 0;
+            int max = 0;
+
+            for (int swipeId : arr) {
+                if (currentInside.contains(swipeId)) {
+                    currentInside.remove(swipeId);
+                    Count--;
+                } else {
+                    currentInside.add(swipeId);
+                    Count++;
+                    if (Count > max) {
+                        max = Count;
+                    }
+                }
+            }
+            return max;
+        }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the value of n");
         int n=sc.nextInt();
         int[] arr=new int[n];
-        int [] arr1=new int[n];
-        int count=1;
         System.out.println("Enter the element");
         for(int i=0;i<n;i++)
         {
             arr[i]= sc.nextInt();
         }
-        for(int i=0;i<arr.length-1;i++)
-        {
-            if(arr[i]!=arr[i+1])
-            {
-                arr1[i]=arr[i];
-            }
-            System.out.println(arr1[i]);
-        }
-        System.out.println(count);
+        System.out.println(maxPeopleInOffice(arr));
     }
 }
