26df3b102d5a04d935747314cea641729ecd673f
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-06-08 12:24:20 +0530

    airport

---
+++ src/CodeChef/StrightRate.java
@@ -0,0 +1,23 @@
+package CodeChef;
+
+import java.util.Scanner;
+
+public class StrightRate {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        double run=0,ball=0,count=0;
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            run+=arr[i];
+            ball++;
+            double d=run/ball;
+            if(d==1)
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
