e10c54f6582e6b22a83c9d8b400f4d47c0cd4164
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-06-11 21:39:27 +0530

    two range

---
+++ src/CodeChef/TwoRange.java
@@ -0,0 +1,30 @@
+package CodeChef;
+
+import java.util.Scanner;
+
+public class TwoRange {
+    public static void main(String[] args) {
+  Scanner sc=new Scanner(System.in);
+
+  int a=sc.nextInt();
+                    int b=sc.nextInt();
+                    int c=sc.nextInt();
+                    int d=sc.nextInt();
+                    int max=Math.max(Math.max(a, b), Math.max(c, d));
+                    int min=Math.min(Math.min(a, b), Math.min(c, d));
+                    int count=0;
+                    for(int i=min; i<=max; i++){
+                        if(a<=b){
+                            count++;
+                            a++;
+                        }
+                        else if(c<=d){
+                            count++;
+                            c++;
+                        }
+                    }
+                    System.out.println(count);
+
+                // your code goeshe
+    }
+}
