ec9084d04f03330849cc7b2d42909c2bb5b468c7
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-06-20 14:05:16 +0530

    two range

---
+++ src/CodeChef/sort.java
@@ -0,0 +1,30 @@
+package CodeChef;
+
+import java.util.Scanner;
+
+public class sort {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        int[] a=new int [n];
+        int[] a1=new int [n];
+        for(int i=0; i<n; i++){
+            a[i]=sc.nextInt();
+
+        }
+        for(int i=0; i<a.length; i++){
+
+            for(int j=i+1; j<a.length; j++){
+                if(a[i]>a[j]){
+                    a1[i]=a[j];
+                }
+
+            }
+        }
+        for(int i=0;i<a.length;i++)//vbbvbfv
+        {
+            System.out.print(a1[i]+" ");
+        }
+    }
+}
