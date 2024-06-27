5319e703095f544215254887348bc11c92511cfc
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-06-27 23:09:44 +0530

    two range

--- src/array/AddMatrix.java
+++ src/array/AddMatrix.java
@@ -47,7 +47,7 @@ public class AddMatrix {
                 System.out.print(arr1[i][j]+" ");
             }
         }
-        System.out.println("Add of two Matrix:");
+        System.out.println("Add of two Matrix:");//huhu
         for(int i=0;i<m;i++)
         {
             for (int j=0;j<n;j++)
