f53dc4da960e3d697161a66d924d20052fdce3e3
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-07-04 19:13:27 +0530

    js function

--- src/hackerRank/StarPattern.java
+++ src/hackerRank/StarPattern.java
@@ -4,21 +4,44 @@ import java.util.Scanner;
 
 public class StarPattern {
     public static void main(String[] args) {
-       // Scanner sc=new Scanner(System.in);
-        for(int i=1;i<=7;i++)
+       Scanner sc=new Scanner(System.in);
+//        for(int i=1;i<=7;i++)
+//        {
+//            for(int j=1;j<=10;j++)
+//            {
+//                if(i==3||i-j==2||i+j==13||i==6)//jgftf
+//                {
+//                    System.out.print("*");
+//                }
+//                else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
+
+
+
+
+
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int area=n*m;
+        int max=0,min=100;
+        for(int i=2;i<=n;i++)
         {
-            for(int j=1;j<=10;j++)
+            if(area%(i*i)==0)
             {
-                if(i==3||i-j==2||i+j==13||i==6)//jgftf
+                max=area/(i*i);
+                if(min>max)
                 {
-                    System.out.print("*");
+                    min=max;
+
                 }
-                else {
-                    System.out.print(" ");
-                }
+
             }
-            System.out.println();
         }
+        System.out.println(min);
 
     }
 }
