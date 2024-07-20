ecef1abdf1ff8e4162b0047ad0c1bf40efc415fe
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-07-20 23:59:35 +0530

    collection

---
+++ src/pattarn/RigTriFectorial.java
@@ -0,0 +1,22 @@
+package pattarn;
+import java.util.Scanner;
+public class RigTriFectorial {
+    public static void rigTriFecto(int n)
+    {
+        int sum=0;
+     for(int i=1;i<=n;i++)
+     {
+         for(int j=i;j<=n;j++)
+         {
+
+         }
+         System.out.println();
+     }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no. you want to print");
+        int n=sc.nextInt();
+        rigTriFecto(n);
+    }
+}
