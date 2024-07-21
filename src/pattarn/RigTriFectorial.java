403666ab0d3ab97c8b99dbcd4eff1ee55585ce72
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-07-21 20:49:36 +0530

    pattern

--- src/pattarn/RigTriFectorial.java
+++ src/pattarn/RigTriFectorial.java
@@ -2,13 +2,12 @@ package pattarn;
 import java.util.Scanner;
 public class RigTriFectorial {
     public static void rigTriFecto(int n)
-    {
+    { long sum=1;
      for(int i=1;i<=n;i++)
      {
          for(int j=1;j<=i;j++)
          {
-             int sum=1;
-             int k=j;
+             int k=i;
              while(k>0) {
                   sum =sum* k;
                  k--;
@@ -19,13 +18,15 @@ public class RigTriFectorial {
      }
     }
     public static void rigTriFecto2(int n)
-    {
+    {int count=1;
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=i;j++)
             {
-
+                System.out.print(count+" ");
+                count+=1;
             }
+            System.out.println();
         }
     }
     public static void main(String[] args) {
