0538fa6146104aff9e8d28f7875fd167d13dcec2
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-07-21 18:35:50 +0530

    pattern

--- src/pattarn/RigTriFectorial.java
+++ src/pattarn/RigTriFectorial.java
@@ -18,10 +18,21 @@ public class RigTriFectorial {
          System.out.println();
      }
     }
+    public static void rigTriFecto2(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+
+            }
+        }
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the no. you want to print");
         int n=sc.nextInt();
         rigTriFecto(n);
+        rigTriFecto2(n);
     }
 }
