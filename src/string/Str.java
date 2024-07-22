16c9f5cd50f3eb278add070564eef0dfcdd81555
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-07-22 15:32:24 +0530

    string

---
+++ src/string/Str.java
@@ -0,0 +1,20 @@
+package string;
+
+public class Str {
+    public static void main(String[] args) {
+
+        String name = "rohit";
+        String name1 = "rohit";
+        String name2 = new String("rohit");
+        if(name==name1){
+            System.out.println("both are same");
+        }
+        if(name1.equals(name2))
+        {
+            System.out.println(" value both are same");
+        }
+        else {
+            System.out.println("both are not same");
+        }
+    }
+}
