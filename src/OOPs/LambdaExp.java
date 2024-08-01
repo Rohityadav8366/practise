c54e2cdae87785a694b9a3dddd54aad70e1f1793
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-08-01 21:59:38 +0530

    Lambda Exprestion

---
+++ src/OOPs/LambdaExp.java
@@ -0,0 +1,20 @@
+package OOPs;
+
+import java.util.Scanner;
+
+public class LambdaExp {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Here the lambda Exprestion");
+
+    }
+}
+class Animal{
+    int age;
+    String name;
+    static void animal(int age,String name){
+        System.out.println();
+    }
+
+}
+
