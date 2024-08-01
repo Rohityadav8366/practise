68219c412bfbcbfeef948155fe5eb51c771683c3
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-08-01 22:16:48 +0530

    Lambda Exprestion

--- src/filehand/swape.java
+++ src/filehand/swape.java
@@ -9,6 +9,7 @@ public class swape {
         int a= sc.nextInt();
         int b= sc.nextInt();
         System.out.println("Swaped number:");
+        // hgytdursgf
         a=a+b;
         b=a-b;
         a=a-b;
