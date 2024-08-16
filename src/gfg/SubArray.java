ee092f4093029b97a3bc4254f6008b3d6b5332bb
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-08-16 16:28:13 +0530

    Abstraction

---
+++ src/gfg/SubArray.java
@@ -0,0 +1,49 @@
+package gfg;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SubArray {
+    public static void subarraySum(int[] arr, int n, int s)
+    {
+        while(true) {
+            int sum = 0;
+            int start = 0;
+            int i;
+            for ( i = 0; i < n; i++) {
+                if (sum == 0) {
+                    start = i + 1;
+                }
+                sum = sum + arr[i];
+                if (sum == s) {
+                    System.out.println(start + "," + i);
+                }
+            }
+            if (sum > s) {
+                sum = 0;
+                i = start;
+                start = 0;
+
+            }
+            else {
+                System.out.println("-1");
+                System.exit(0);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        //BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int s=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        subarraySum(arr,n,s);
+    }
+}
