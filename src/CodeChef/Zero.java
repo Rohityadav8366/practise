5b0130eeea1336c6fc49c70c74ab41cb0b064450
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-08-29 02:27:49 +0530

    Encpsulation

---
+++ src/CodeChef/Zero.java
@@ -0,0 +1,15 @@
+package CodeChef;
+
+import java.util.Scanner;
+
+public class Zero {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+       // System.out.println("n >");
+        int n=sc.nextInt();
+       // System.out.println("Enter the sring");
+        String str=sc.nextLine();
+        str.length();
+
+    }
+}
