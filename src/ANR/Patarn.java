11cde2eb09cd6e9b12496f13b3672e42bea7e2e2
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-09-10 00:10:41 +0530

    Anr prepration

--- src/ANR/Patarn.java
+++ src/ANR/Patarn.java
@@ -1,17 +1,37 @@
 package ANR;
 
 public class Patarn {
-    public static void main(String arg[]) {
+
+    public static void squire(){
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
                 if (i==1||j==1||j==5||i==5 ) {
                     System.out.print("* ");
-               }
+                }
                 else{
                     System.out.print("  ");
                 }
             }
             System.out.println();
         }
+    }
+
+    public static void triangle(){
+        for (int i = 1; i <= 5; i++) {
+            for (int j = i; j <= 5; j++) {
+//                if (true ) {
+//                    System.out.print("* ");
+//                }
+//                else{
+//                    System.out.print("  ");
+//                }
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String arg[]) {
+     // squire();
+        triangle();
     }
 }
