c6a497774d5f3ae7e6abedc5f151fdb2a4ed66f8
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-09-09 23:59:09 +0530

    Anr prepration

--- src/ANR/Patarn.java
+++ src/ANR/Patarn.java
@@ -4,9 +4,12 @@ public class Patarn {
     public static void main(String arg[]) {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5; j++) {
-//                if (i==1 ) {
+                if (i==1||j==1||j==5||i==5 ) {
                     System.out.print("* ");
-              //  }
+               }
+                else{
+                    System.out.print("  ");
+                }
             }
             System.out.println();
         }
