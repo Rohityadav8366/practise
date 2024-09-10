cc3e0c99b83ead3f469091d061e94f218328346b
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-09-10 23:23:52 +0530

    Pattern

--- src/ANR/Patarn.java
+++ src/ANR/Patarn.java
@@ -18,20 +18,35 @@ public class Patarn {
 
     public static void triangle(){
         for (int i = 1; i <= 5; i++) {
-            for (int j = i; j <= 5; j++) {
-//                if (true ) {
-//                    System.out.print("* ");
-//                }
-//                else{
-//                    System.out.print("  ");
-//                }
-                System.out.print("* ");
+            for (int j = 1;j<=5;j++) {
+                if ( i==5||j==5||(i==2)&&(j==4)||(i==3)&&(j==4||j==3)||(i==4)&&(j==4||j==3||j==2)) {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+                //System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void triangle1(){
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1;j<=5;j++) {
+                if ( i==1||j==1||(i==2)&&(j==4)||(i==3)&&(j==4||j==3)||(i==4)&&(j==4||j==3||j==2)) {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+                //System.out.print("* ");
             }
             System.out.println();
         }
     }
     public static void main(String arg[]) {
      // squire();
-        triangle();
+        triangle1();
     }
 }
