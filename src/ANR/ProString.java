5e6bc94a9dc084ec9173002a3a760298a55ddd70
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-09-19 18:23:39 +0530

    Anr prepration String

--- src/ANR/ProString.java
+++ src/ANR/ProString.java
@@ -5,26 +5,38 @@ import java.util.Scanner;
 public class ProString {
 
 //char from string
-        public static void stringToChar(String str) {
+        public static void reverseString(String str) {
+            char[] chArr= str.toCharArray();
+            StringBuffer sb=new StringBuffer(str);
+            StringBuilder stringBuilder=new StringBuilder(str);
             for (int i = str.length() - 1; i >= 0; i--) {
-                System.out.print(str.charAt(i));
+//                System.out.print(chArr[i]);
+//                System.out.print(str.charAt(i));
             }
+            System.out.println(sb.reverse());
+
+            System.out.println(stringBuilder.reverse());
+
         }
 //Pallindrom string
     public static void pallindrome(String str)
     {
-        String str1="";
-        for(int i=str.length()-1;i>0;i--)
+        char[] chArr=new char[str.length()];
+        char ch = 0;
+        for(int i=str.length()-1;i>=0;i--)
         {
-             str1 = String.valueOf(str.charAt(i));
+              ch =str.charAt(i);
+        }
+        for(int i=0;i<str.length();i++){
+            chArr[i]=ch;
         }
         System.out.println(str);
-        System.out.println(str1);
+        System.out.println(chArr);
     }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String str=sc.nextLine();
-        stringToChar(str);
+        pallindrome(str);
     }
 }
