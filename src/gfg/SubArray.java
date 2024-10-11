26ed457dc07aa445fe9e84d9261ab590ac4733d2
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-10-11 23:49:11 +0530

    gfg array code

--- src/gfg/SubArray.java
+++ src/gfg/SubArray.java
@@ -19,22 +19,47 @@ public class SubArray {
       }
 
       public static void stockBuyAndShell(){
-
+        Scanner sc=new Scanner(System.in);
+          System.out.println("Enter the size of array:");
+        int n=sc.nextInt();
+          System.out.println("Enter the element of array: ");
+        int[] arr=new int[n];
+        int min=0;
+        int p=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            if(i==0){
+            min=arr[0];}
+            if(min>arr[i]){
+                min=arr[i];
+                p=i;
+            }
+        }
+        int max=0;
+        for(int i=p+1;i<n;i++){
+            if(i==p+1){
+            max=arr[p+1]-min;}
+            int pro=arr[i]-min;
+            if(max<pro){
+                max=pro;
+            }
+        }
+          System.out.println("Maximum profit is "+max);
       }
 
 
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Entre the N:");
-        int n=sc.nextInt();
-        System.out.println("Entre the s:");
-        int s=sc.nextInt();
-        System.out.println("Entre the element:");
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        subarraySum(arr,n,s);
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Entre the N:");
+//        int n=sc.nextInt();
+//        System.out.println("Entre the s:");
+//        int s=sc.nextInt();
+//        System.out.println("Entre the element:");
+//        int[] arr=new int[n];
+//        for(int i=0;i<n;i++){
+//            arr[i]=sc.nextInt();
+//        }
+        stockBuyAndShell();
     }
 }
