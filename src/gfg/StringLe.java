e8b73824aea1e6f13eb89ebd89d9cee5e70d7cd8
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-11-13 00:11:40 +0530

    code practise

--- src/gfg/StringLe.java
+++ src/gfg/StringLe.java
@@ -26,13 +26,27 @@ public class StringLe {
         }
     }
 
-    public static void prime(){
-        Scanner sc=new Scanner(System.in);
-        int n =sc.nextInt();
-
+    public static void prime() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        if (n == 1) {
+            System.out.println("Co-prime" + n);
+        } else {
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    count++;
+                }
+            }
+            if (count > 0) {
+                System.out.println("Not prime" + n);
+            } else {
+                System.out.println("PRime " + n);
+            }
+        }
     }
-
     public static void main(String[] args) {
-        reverseString();
+       // reverseString();
+        prime();
     }
 }
