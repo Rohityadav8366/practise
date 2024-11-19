f8a40fd0dc5ca7830d71ee1097f7f84488fc6023
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2024-11-19 23:54:50 +0530

    MaxSum from Array to subArray

--- src/array/maxSum.java
+++ src/array/maxSum.java
@@ -1,8 +1,27 @@
 package array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class maxSum {
+    public static void MaxSum(int[] arr){
+        int sum=0;
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<0){
+                continue;
+            } else if (i==0) {
+                continue;
+            } else{
+                sum=sum+arr[i];
+            }
+        }
+        System.out.println(sum);
+    }
+
+    public static void minJumps(int[] arr){
+
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the size of array");
@@ -12,5 +31,6 @@ public class maxSum {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
+        MaxSum(arr);
     }
 }
