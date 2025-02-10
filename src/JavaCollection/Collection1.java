26dee19c004d2492e52febddf22a737eeccfea2b
Rohit Kumar Yadav <124360033+Rohityadav8366@users.noreply.github.com>
2025-02-11 00:07:18 +0530

    start

---
+++ src/JavaCollection/Collection1.java
@@ -0,0 +1,14 @@
+package JavaCollection;
+
+import java.util.Scanner;
+
+public class Collection1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+    }
+}
